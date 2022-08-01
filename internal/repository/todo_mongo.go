cda028e5c20b96308470c14d9cd5d2ffb376ea6a
Indigowar <indigowar.github@gmail.com>
2022-08-01 17:58:37 +0300

    create mongo entities for todo repository.

--- internal/repository/todo_mongo.go
+++ internal/repository/todo_mongo.go
@@ -13,6 +13,48 @@ func NewTodoRepo(database *mongo.Database) (TodoRepo, error) {
 	}, nil
 }
 
+type mongoElement struct {
+	ID string `bson:"_id"`
+	ElementValue string `bson:"value"`
+	Status  bool `bson:"done"`
+}
+func (e *mongoElement) Id() uuid.UUID {
+	id, _ := uuid.Parse(e.ID)
+	return id
+}
+func (e *mongoElement) Value() string {
+	return e.ElementValue
+}
+func (e *mongoElement) Done() bool {
+	return e.Status
+}
+
+type mongoList struct {
+	ID string `bson:"_id"`
+	ListName     string `bson:"name"`
+	ListOwner    string `bson:"owner"`
+	ElementsID []string `bson:"elements"`
+}
+func (l *mongoList) Id() uuid.UUID {
+	id, _ := uuid.Parse(l.ID)
+	return id
+}
+func (l *mongoList) Name() string {
+	return l.ListName
+}
+func (l *mongoList) Owner() uuid.UUID {
+	id, _ := uuid.Parse(l.ListOwner)
+	return id
+}
+func (l *mongoList) Elements() []uuid.UUID {
+	result := make([]uuid.UUID, len(l.ElementsID))
+	for i, v := range l.ElementsID {
+		id, _ := uuid.Parse(v)
+		result[i] = id
+	}
+	return result
+}
+
 type todoMongoRepo struct {
 	lists *mongo.Collection
 	elements *mongo.Collection
