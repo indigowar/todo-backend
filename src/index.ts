3b7e2c7923885344a690159332fef5d08587ca07
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-10-15 17:46:38 +0300

    reformat

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import UserSvc from './services/user';
 import HttpInfra from './infra/ports/http/http';
 
 (async () => {
-  const user_service: UserService = new UserSvc();
-  const http_infra = new HttpInfra(user_service);
-  http_infra.run();
+	const user_service: UserService = new UserSvc();
+	const http_infra = new HttpInfra(user_service);
+	http_infra.run();
 })();
