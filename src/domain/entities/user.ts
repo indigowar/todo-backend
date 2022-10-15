431da09e54c0de0bd56cddc44b1ed9823461d426
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-10-15 17:45:27 +0300

    Update user.ts
    Add getters and setters .

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,33 +1,38 @@
 import ValidValue from '../../utils/validValue';
 
 export default class User {
-  private id: string;
-  private name: UserName;
-  private password: UserPassword;
+	private readonly _id: string;
 
-  constructor (id: string, name: UserName, password: UserPassword) {
-	this.id = id;
-	this.name = name;
-	this.password = password;
-  }
+	constructor (id: string, name: UserName, password: UserPassword) {
+		this._id = id;
+		this.name = name;
+		this.password = password;
+	}
+
+	public name: UserName;
+	public password: UserPassword;
+
+	public get id () {
+		return this._id;
+	}
 }
 
 export class UserPassword extends ValidValue<string> {
-  constructor (v: string) {
-	super(v);
-  }
+	constructor (v: string) {
+		super(v);
+	}
 
-  public valid (v: string): boolean {
-	return true;
-  }
+	public valid (v: string): boolean {
+		return true;
+	}
 }
 
 export class UserName extends ValidValue<string> {
-  constructor (value: string) {
-	super(value);
-  }
+	constructor (value: string) {
+		super(value);
+	}
 
-  public valid (v: string): boolean {
-	return true;
-  }
+	public valid (v: string): boolean {
+		return true;
+	}
 }
